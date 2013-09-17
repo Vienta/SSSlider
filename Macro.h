19beadb242d83b04bafd99892877a3254ab0c80b
Vienta <yongxingshu@foxmail.com>
2013-09-18 00:06:06 +0800

    vi+macro

---
+++ Macro.h
@@ -0,0 +1,14 @@
+//
+//  Macro.h
+//  macro
+//
+//  Created by 束 永兴 on 13-9-17.
+//  Copyright (c) 2013年 RayAse. All rights reserved.
+//
+
+#ifndef macro_Macro_h
+#define macro_Macro_h
+
+#define iPhone4Inch CGSizeEqualToSize([[UIScreen mainScreen] preferredMode].size,CGSizeMake(640, 1136))
+
+#endif
